d1b4d6d8f8dabf1e92dacf1cff09090ce24c4181
Juhun Park <riann3207@gmail.com>
2024-06-27 12:43:38 -0400

    Birthday Cake Candles

---
+++ python/Birthday Cake Candles/main.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    hashmap = {}
+    for candle in candles:
+        if candle in hashmap:
+            hashmap[candle] += 1
+        else:
+            hashmap[candle] = 1
+    
+    print(hashmap)
+    return hashmap[max(hashmap)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
